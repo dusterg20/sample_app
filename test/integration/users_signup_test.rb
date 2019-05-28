26cf34aa639c39775c14cc2e9c88e3217611e9dc
Dustin Garrett <dusterg20@gmail.com>
2019-05-28 02:36:41 +0000

    Implement Basic Login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,8 +8,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
   
   test "valid signup information" do
@@ -19,6 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
   
 end
